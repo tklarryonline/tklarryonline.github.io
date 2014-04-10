63701e069f51d1fa83acf5e83cdb2437d68b997c
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-04-10 16:34:58 +0700

    Stopped using flat shadow

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,2 @@
-function bindLogo() {
-	$("#tk-logo.flat-icon").flatshadow({
-		// Shadows direction. 
-		// Available options: N, NE, E, SE, S, SW, W and NW.
-		// (Angle will be random if left unassigned)
-		// Gradient shadow effect
-		fade: false 
-	});
-}
-
 $(function() {
-	bindLogo();
 });
-
