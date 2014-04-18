862d5c0a798e320107a99f52f642c93623e2a696
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-04-18 17:55:22 +0700

    Updated blog roll design

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,9 @@
-$(function() {
+function renderPostDatetime() {
     return $.each($("[data-time]"), function(index, ele) {
         return $(ele).html(prettyDate(new Date($(this).data("time"))));
     });
+}
+
+$(function() {
+    renderPostDatetime();
 });
