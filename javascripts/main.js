b92e885a19ba734d3e76dcb103ca5a36fd236c97
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-04-20 23:04:25 +0700

    Improved readability

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 function renderPostDatetime() {
     return $.each($("[data-time]"), function(index, ele) {
-        return $(ele).html(prettyDate(new Date($(this).data("time"))));
+        return $(ele).html(prettyDate(new Date($(this).data("time"))).toLowerCase());
     });
 }
 
